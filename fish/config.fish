de09764180fd5526d3471c527098d72d233d2ef0
HxprLee <93396916+HxprLee@users.noreply.github.com>
2026-01-08 13:50:37 +0700

    realize the symlinks didn't work

---
+++ fish/config.fish
@@ -0,0 +1,15 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+set -g fish_greeting ''
+
+fish_add_path /home/lee/.spicetify
+
+# Created by `pipx` on 2025-11-11 16:49:28
+set PATH $PATH /home/lee/.local/bin
+
+# Added by LM Studio CLI (lms)
+set -gx PATH $PATH /home/lee/.lmstudio/bin
+# End of LM Studio CLI section
+
